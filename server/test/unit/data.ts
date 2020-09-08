56cee6690d2ccc1be2a63cf1b3e934f0d4d337b9
Kartik Patel <kartik.patel@openlane.com>
2020-09-08 17:05:17 -0400

    Add test case for upload and getReport

--- server/test/unit/data.ts
+++ server/test/unit/data.ts
@@ -61,5 +61,37 @@ export default {
         hours_worked: '5',
         employee_id: '4',
         job_group: 'B'
-    }]
+    }],
+    PAYROLL_SERVICE_REPORT_SERVICE_RESPONSE: [
+        { "employeeId": "1", "payPeriod": { "startDate": "2016-11-01", "endDate": "2016-11-15" }, "amountPaid": "150.0" },
+        { "employeeId": "1", "payPeriod": { "startDate": "2016-11-16", "endDate": "2016-11-30" }, "amountPaid": "220" },
+        { "employeeId": "1", "payPeriod": { "startDate": "2016-12-01", "endDate": "2016-12-15" }, "amountPaid": "150.0" },
+        { "employeeId": "1", "payPeriod": { "startDate": "2016-12-16", "endDate": "2016-12-31" }, "amountPaid": "220" },
+        { "employeeId": "2", "payPeriod": { "startDate": "2016-11-01", "endDate": "2016-11-15" }, "amountPaid": "930" },
+        { "employeeId": "2", "payPeriod": { "startDate": "2016-12-01", "endDate": "2016-12-15" }, "amountPaid": "930" },
+        { "employeeId": "3", "payPeriod": { "startDate": "2016-11-01", "endDate": "2016-11-15" }, "amountPaid": "590.0" },
+        { "employeeId": "3", "payPeriod": { "startDate": "2016-12-01", "endDate": "2016-12-15" }, "amountPaid": "470.0" },
+        { "employeeId": "4", "payPeriod": { "startDate": "2015-02-16", "endDate": "2015-02-28" }, "amountPaid": "100" },
+        { "employeeId": "4", "payPeriod": { "startDate": "2016-02-16", "endDate": "2016-02-29" }, "amountPaid": "150" },
+        { "employeeId": "4", "payPeriod": { "startDate": "2016-11-01", "endDate": "2016-11-15" }, "amountPaid": "150" },
+        { "employeeId": "4", "payPeriod": { "startDate": "2016-11-16", "endDate": "2016-11-30" }, "amountPaid": "450" },
+        { "employeeId": "4", "payPeriod": { "startDate": "2016-12-01", "endDate": "2016-12-15" }, "amountPaid": "150" },
+        { "employeeId": "4", "payPeriod": { "startDate": "2016-12-16", "endDate": "2016-12-31" }, "amountPaid": "450" }
+    ],
+    PAYROLL_SERVICE_REPORT_QUERY_DATA: [
+        { "employee_id": "1", "start_date": "2016-11-01", "end_date": "2016-11-15", "amount_paid": "150.0" },
+        { "employee_id": "1", "start_date": "2016-11-16", "end_date": "2016-11-30", "amount_paid": "220" },
+        { "employee_id": "1", "start_date": "2016-12-01", "end_date": "2016-12-15", "amount_paid": "150.0" },
+        { "employee_id": "1", "start_date": "2016-12-16", "end_date": "2016-12-31", "amount_paid": "220" },
+        { "employee_id": "2", "start_date": "2016-11-01", "end_date": "2016-11-15", "amount_paid": "930" },
+        { "employee_id": "2", "start_date": "2016-12-01", "end_date": "2016-12-15", "amount_paid": "930" },
+        { "employee_id": "3", "start_date": "2016-11-01", "end_date": "2016-11-15", "amount_paid": "590.0" },
+        { "employee_id": "3", "start_date": "2016-12-01", "end_date": "2016-12-15", "amount_paid": "470.0" },
+        { "employee_id": "4", "start_date": "2015-02-16", "end_date": "2015-02-28", "amount_paid": "100" },
+        { "employee_id": "4", "start_date": "2016-02-16", "end_date": "2016-02-29", "amount_paid": "150" },
+        { "employee_id": "4", "start_date": "2016-11-01", "end_date": "2016-11-15", "amount_paid": "150" },
+        { "employee_id": "4", "start_date": "2016-11-16", "end_date": "2016-11-30", "amount_paid": "450" },
+        { "employee_id": "4", "start_date": "2016-12-01", "end_date": "2016-12-15", "amount_paid": "150" },
+        { "employee_id": "4", "start_date": "2016-12-16", "end_date": "2016-12-31", "amount_paid": "450" }
+    ]
 }
--- server/test/unit/payroll-service.test.ts
+++ server/test/unit/payroll-service.test.ts
@@ -4,17 +4,12 @@ import * as sinon  from 'sinon';
 import { DBService } from '../../src/main/utils/db-init';
 import { PayrollService } from '../../src/main/service/payroll-service';
 import { Payroll } from '../../src/main/model/payroll';
-// import { Parse } from '../../src/main/utils/csv-parser';
-
 import data from './data';
 
 describe('Payroll Service', () => {
 
     let service;
-    // let dbClientStub;
     let dbClient;
-    // let payrollStub;
-    // let DBConnectionMock;
 
     beforeEach(() => {
         dbClient = DBService.createDBClient();
@@ -22,9 +17,19 @@ describe('Payroll Service', () => {
     });
 
     it('should save the data to the database', async () => {
-        // sinon.stub(dbClient, 'authenticate').returns(true);
         const upsertStub = sinon.stub(Payroll, 'upsert').returns(true);
         service.saveData(data.PAYROLL_SERVICE_UPLOAD_TEST_DATA);
         expect(upsertStub.callCount).toBe(10);
+    });
+
+    it('should return the payroll report.', async () => {
+        sinon.stub(dbClient, 'query').resolves(data.PAYROLL_SERVICE_REPORT_QUERY_DATA);
+        const report = await service.generateReport();
+        const expectedOutput = {
+            "payrollReport": {
+                "employeeReports": data.PAYROLL_SERVICE_REPORT_SERVICE_RESPONSE
+            }
+        }
+        expect(report).toEqual(expectedOutput);
     });
 });
